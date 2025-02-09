9946d4ba120e92c27900d6346d33c3ce9d1d3bdf
tlipinski <me@tlipinski.net>
2025-02-09 11:02:42 +0100

    config

---
+++ src/config.rs
@@ -0,0 +1,30 @@
+use anyhow::Result;
+use log::info;
+use secrecy::SecretBox;
+use serde::Deserialize;
+use std::env;
+use std::fs::OpenOptions;
+use std::io::Read;
+use std::path::PathBuf;
+
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    pub api: Api,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Api {
+    pub key: SecretBox<String>,
+}
+
+pub fn get_config() -> Result<Config> {
+    info!("Loading config");
+    let home = env::var("HOME")?;
+    let config_path = PathBuf::from(home).join(".config/subster/config.toml");
+    let mut opened = OpenOptions::new().read(true).open(config_path)?;
+    let mut contents = String::new();
+    opened.read_to_string(&mut contents)?;
+    let c: Config = toml::from_str(&contents)?;
+    info!("Config loaded {:?}", c);
+    Ok(c)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
-use anyhow::Result;
+mod config;
+
+use crate::config::get_config;
 use log::{error, info};
-use secrecy::SecretBox;
-use serde::Deserialize;
-use std::env;
-use std::fs::OpenOptions;
-use std::io::Read;
-use std::path::PathBuf;
 
 #[tokio::main]
 async fn main() {
@@ -19,26 +15,4 @@ async fn main() {
             error!("{:?}", e);
         }
     };
-}
-
-#[derive(Debug, Deserialize)]
-pub struct Config {
-    api: Api,
-}
-
-#[derive(Debug, Deserialize)]
-struct Api {
-    key: SecretBox<String>,
-}
-
-fn get_config() -> Result<Config> {
-    info!("Loading config");
-    let home = env::var("HOME")?;
-    let config_path = PathBuf::from(home).join(".config/subster/config.toml");
-    let mut opened = OpenOptions::new().read(true).open(config_path)?;
-    let mut contents = String::new();
-    opened.read_to_string(&mut contents)?;
-    let c: Config = toml::from_str(&contents)?;
-    info!("Config loaded {:?}", c);
-    Ok(c)
 }
