8f1a137933cc9d173b142bf0068450c4af277879
tlipinski <me@tlipinski.net>
2025-02-16 17:25:46 +0100

    Switch to GET request and enhance `GuessResponse` structure

--- src/guess.rs
+++ src/guess.rs
@@ -1,69 +1,83 @@
-use std::collections::HashMap;
+use crate::login::ApiToken;
+use crate::values::{API_URL, KEY, USER_AGENT};
 use anyhow::{Error, Result};
 use log::{debug, error, info};
 use serde::Deserialize;
-use crate::login::ApiToken;
-use crate::values::{API_URL, KEY, USER_AGENT};
+use std::collections::HashMap;
 
 pub async fn guess(title: &str) -> Result<GuessResponse> {
-  info!("Searching for {title}");
-  let url = format!("{}/utilities/guessit", API_URL);
+    info!("Searching for {title}");
+    let url = format!("{}/utilities/guessit", API_URL);
 
-  let mut query_params = HashMap::new();
-  query_params.insert("filename", title);
+    let mut query_params = HashMap::new();
+    query_params.insert("filename", title);
 
-  let req = reqwest::Client::new()
-      .post(url)
-      .header("Api-Key", KEY.clone())
-      .header("User-Agent", USER_AGENT)
-      .query(&query_params);
+    let req = reqwest::Client::new()
+        .get(url)
+        .header("Api-Key", KEY.clone())
+        .header("User-Agent", USER_AGENT)
+        .query(&query_params);
 
-  println!("{:?}", req);
+    println!("{:?}", req);
 
-  let response = req.send().await?;
+    let response = req.send().await?;
 
-  let status = response.status();
+    let status = response.status();
 
-  println!("{}", status);
+    println!("{}", status);
 
-  let text_body = response.text().await?;
+    let text_body = response.text().await?;
 
-  println!("{}", text_body);
+    println!("{}", text_body);
 
-  match status {
-    s if s.is_success() || s.is_redirection() => {
-      debug!("Response {}", text_body);
-      let json: Result<GuessResponse, _> = serde_json::from_str(&text_body);
-      match json {
-        Ok(guess_response) => {
-          debug!("{:?}", guess_response);
-          Ok(guess_response)
+    match status {
+        s if s.is_success() || s.is_redirection() => {
+            debug!("Response {}", text_body);
+            let json: Result<GuessResponse, _> = serde_json::from_str(&text_body);
+            match json {
+                Ok(guess_response) => {
+                    debug!("{:?}", guess_response);
+                    Ok(guess_response)
+                }
+                Err(e) => {
+                    error!("Failed decoding body {:?} {}", e, text_body);
+                    Err(Error::from(e))
+                }
+            }
         }
-        Err(e) => {
-          error!("Failed decoding body {:?} {}", e, text_body);
-          Err(Error::from(e))
+        s if s.is_client_error() => {
+            let error_response: crate::login::ErrorResponse = serde_json::from_str(&text_body)?;
+            info!("Client error {:?}", error_response);
+            if error_response.message.contains("invalid username/password") {
+                Err(Error::msg("Invalid username or password"))
+            } else {
+                Err(Error::msg("Error calling OSB"))
+            }
         }
-      }
+        s => {
+            error!("Server error [{}]: {}", s.as_u16(), text_body);
+            Err(Error::msg("Server error"))
+        }
     }
-    s if s.is_client_error() => {
-      let error_response: crate::login::ErrorResponse = serde_json::from_str(&text_body)?;
-      info!("Client error {:?}", error_response);
-      if error_response.message.contains("invalid username/password") {
-        Err(Error::msg("Invalid username or password"))
-      } else {
-        Err(Error::msg("Error calling OSB"))
-      }
-    }
-    s => {
-      error!("Server error [{}]: {}", s.as_u16(), text_body);
-      Err(Error::msg("Server error"))
-    }
-  }
 }
 
 #[derive(Deserialize, Debug)]
 pub struct GuessResponse {
-  title: String,
-  container: String,
-  episode: i32,
+    title: String,
+    year: Option<i32>,
+    language: Option<String>,
+    subtitle_language: Option<String>,
+    screen_size: Option<String>,
+    streaming_service: Option<String>,
+    source: Option<Vec<String>>,
+    other: Option<String>,
+    audio_codec: Option<String>,
+    audio_channels: Option<String>,
+    video_codec: Option<String>,
+    release_group: Option<String>,
+    container: Option<String>,
+    r#type: Option<String>,
+
+    episode: Option<i32>,
+    season: Option<i32>,
 }
