bcdaed5e3e80cd1b082ae33430506ea9a655fc06
tlipinski <me@tlipinski.net>
2025-02-21 23:45:22 +0100

    Update config path to use XDG base directory

--- src/config.rs
+++ src/config.rs
@@ -1,21 +1,18 @@
+use std::fs;
 use anyhow::Result;
 use log::info;
 use serde::Deserialize;
-use std::{env, fs};
-use std::path::PathBuf;
 
 #[derive(Debug, Deserialize)]
-pub struct Config {
-}
+pub struct Config {}
 
 #[derive(Debug, Deserialize)]
-pub struct Api {
-}
+pub struct Api {}
 
 pub fn get_config() -> Result<Config> {
     info!("Loading config");
-    let home = env::var("HOME")?;
-    let config_path = PathBuf::from(home).join(".config/subster/config.toml");
+    let xdg_dirs = xdg::BaseDirectories::with_prefix("subster")?;
+    let config_path = xdg_dirs.get_config_home().join("config.toml");
     let contents = fs::read_to_string(config_path)?;
     let config: Config = toml::from_str(&contents)?;
     info!("Config loaded {:?}", config);
