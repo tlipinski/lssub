1879af262371d5e8c69311697f73f89baf6f604b
tlipinski <me@tlipinski.net>
2025-06-03 09:59:10 +0200

    Refactor SearchWidget input handling for key events
    Simplify input event handling by using the state change returned by `handle_event` to conditionally send updates. This improves efficiency by reducing unnecessary transmissions when the input value remains unchanged. Additionally, minor formatting adjustments were made for consistency.

--- src/ui/search_widget.rs
+++ src/ui/search_widget.rs
@@ -8,8 +8,8 @@ use ratatui::prelude::{Line, Stylize, Widget};
 use ratatui::symbols::border;
 use ratatui::widgets::{Block, Paragraph, StatefulWidget, TableState};
 use std::sync::mpsc::Sender;
-use tui_input::backend::crossterm::EventHandler;
 use tui_input::Input;
+use tui_input::backend::crossterm::EventHandler;
 
 #[derive(Debug)]
 pub struct SearchWidget {
@@ -23,7 +23,7 @@ impl SearchWidget {
         SearchWidget {
             features_tx,
             active: true,
-            input: Input::from(search_text)
+            input: Input::from(search_text),
         }
     }
 
@@ -40,7 +40,7 @@ impl SearchWidget {
         let par = Line::from(self.input.value().bold());
 
         let view = Paragraph::new(par).block(block);
-        
+
         let x = self.input.visual_cursor();
         frame.set_cursor_position((area.x + (x + 1) as u16, area.y + 1));
 
@@ -55,8 +55,11 @@ impl SearchWidget {
     }
 
     pub fn handle_key_event(&mut self, event: Event) -> Result<()> {
-        self.input.handle_event(&event);
-        self.features_tx.send(self.input.value().into())?;
+        if let Some(state_changed) = self.input.handle_event(&event) {
+            if state_changed.value {
+                self.features_tx.send(self.input.value().into())?;
+            }
+        }
         Ok(())
     }
 }
