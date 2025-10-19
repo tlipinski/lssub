be9f3273b7f7dfec390f64312f7337b8987fd616
tlipinski <me@tlipinski.net>
2025-10-19 19:24:49 +0200

    Simplify spinner logic by using array indexing instead of string operations.

--- src/ui/spinner.rs
+++ src/ui/spinner.rs
@@ -4,15 +4,13 @@ use tokio::time::{sleep, Duration};
 use crate::ui::events::UiEvent;
 
 pub async fn handle_spinner(tx: Sender<UiEvent>) -> anyhow::Result<()> {
-    let spinner = "|/-\\";
+    let spinner = ['|', '/', '-', '\\'];
     let mut pos = 0;
     loop {
         sleep(Duration::from_millis(200)).await;
         pos += 1;
-        if pos == 4 {
-            pos = 0;
-        }
-        let ch = spinner.chars().nth(pos).unwrap_or('|');
+        pos = pos % spinner.len();
+        let ch = spinner[pos];
         tx.send(UiEvent::SpinnerUpdate(ch))?
     }
 }
