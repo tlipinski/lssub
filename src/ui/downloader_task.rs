122488cccfd08024ddfa680e47c06890f8e62f74
tlipinski <me@tlipinski.net>
2025-11-11 17:15:56 +0100

    Extract `output_file` logic from `downloader_task` for clarity and reusability. Add unit tests for `output_file` to handle various file name scenarios.

--- src/ui/downloader_task.rs
+++ src/ui/downloader_task.rs
@@ -1,6 +1,7 @@
 use log::info;
 use osb::download::download;
 use osb::get_download_link::get_download_link;
+use std::ffi::{OsStr, OsString};
 use std::fs;
 use std::path::{Path, PathBuf};
 use std::sync::mpsc::Receiver;
@@ -8,7 +9,7 @@ use std::sync::mpsc::Receiver;
 pub async fn downloader_task(
     rx: Receiver<SubsDownload>,
     base_path: PathBuf,
-    file_name: Option<String>,
+    file_name_opt: Option<String>,
 ) -> anyhow::Result<()> {
     loop {
         match rx.recv() {
@@ -19,10 +20,13 @@ pub async fn downloader_task(
 
                 let content = download(download_link_response.link).await?;
 
-                let file_base = file_name.as_deref().unwrap_or(download_link_response.file_name.as_str());
-                let file = Path::new(file_base).with_extension("srt");
+                let output_file = output_file(
+                    &base_path,
+                    &file_name_opt,
+                    download_link_response.file_name.as_str(),
+                );
 
-                fs::write(base_path.join(file), content)?;
+                fs::write(output_file, content)?;
             }
             Err(err) => {
                 info!("Error: {err}");
@@ -32,7 +36,46 @@ pub async fn downloader_task(
     }
 }
 
+fn output_file(
+    base_path: &PathBuf,
+    file_name_opt: &Option<String>,
+    default_file_name: &str,
+) -> PathBuf {
+    let ext = PathBuf::from(default_file_name).extension();
+
+    let file_base = file_name_opt.as_deref().unwrap_or(&default_file_name);
+
+    let file = Path::new(file_base).with_extension("srt");
+
+    base_path.join(file)
+}
+
 #[derive(Debug)]
 pub struct SubsDownload {
     pub file_id: i64,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn no_input_file() {
+        assert_eq!(
+            output_file(&PathBuf::from("/home/user"), &None, "default.ext"),
+            PathBuf::from("/home/user/default.ext")
+        );
+    }
+
+    #[test]
+    fn input_file_with_ext_from_default() {
+        assert_eq!(
+            output_file(
+                &PathBuf::from("/home/user"),
+                &Some(String::from("file")),
+                "default.ext"
+            ),
+            PathBuf::from("/home/user/file.ext")
+        );
+    }
 }
