95077b328d36f48cf1070b26a0ae7ce705af30fd
tlipinski <me@tlipinski.net>
2026-02-22 17:57:18 +0100

    Add editing state to `LoginWidget` for improved input navigation and cursor management

--- src/ui/login_widget.rs
+++ src/ui/login_widget.rs
@@ -13,6 +13,13 @@ use tui_input::backend::crossterm::EventHandler;
 pub struct LoginWidget {
     pub username: Input,
     pub password: Input,
+    editing: Editing,
+}
+
+#[derive(Debug)]
+enum Editing {
+    Username,
+    Password,
 }
 
 impl LoginWidget {
@@ -20,6 +27,7 @@ impl LoginWidget {
         LoginWidget {
             username: Input::new("user".into()),
             password: Input::new("pass".into()),
+            editing: Editing::Username,
         }
     }
 
@@ -28,13 +36,18 @@ impl LoginWidget {
             .title(" Login ".to_string().bold())
             .border_set(border::THICK);
 
-        let user_block = Block::bordered()
-            .title(" Username ")
-            .border_set(border::ROUNDED);
+        let mut user_block = Block::bordered().title(" Username ");
 
-        let pass_block = Block::bordered()
-            .title(" Password ")
-            .border_set(border::ROUNDED);
+        let mut pass_block = Block::bordered().title(" Password ");
+
+        match self.editing {
+            Editing::Username => {
+                user_block = user_block.border_set(border::THICK);
+            }
+            Editing::Password => {
+                pass_block = pass_block.border_set(border::THICK);
+            }
+        }
 
         let buttons = Line::from(vec![
             Span::from("OK").bold(),
@@ -72,18 +85,46 @@ impl LoginWidget {
         frame.render_widget(pass_par, layout[1]);
         frame.render_widget(buttons_block, layout[2]);
 
-        frame.set_cursor_position((
-            layout[0].x + (self.username.visual_cursor() + 1) as u16,
-            area.y + 2,
-        ));
+        match self.editing {
+            Editing::Username => frame.set_cursor_position((
+                layout[0].x + (self.username.visual_cursor() + 1) as u16,
+                layout[0].y + 1,
+            )),
+            Editing::Password => frame.set_cursor_position((
+                layout[1].x + (self.password.visual_cursor() + 1) as u16,
+                layout[1].y + 1,
+            )),
+        };
     }
 
     pub fn handle_key_event(&mut self, event: Event) -> Option<UiMessage> {
         if let Event::Key(key_event) = event {
             match key_event.code {
                 KeyCode::Enter => Some(UiMessage::LoggedIn),
+                KeyCode::Up => {
+                    self.editing = Editing::Username;
+                    None
+                }
+                KeyCode::Down => {
+                    self.editing = Editing::Password;
+                    None
+                }
+                KeyCode::Tab => {
+                    match self.editing {
+                        Editing::Username => self.editing = Editing::Password,
+                        Editing::Password => self.editing = Editing::Username,
+                    }
+                    None
+                }
                 _ => {
-                    self.username.handle_event(&event);
+                    match self.editing {
+                        Editing::Username => {
+                            self.username.handle_event(&event);
+                        }
+                        Editing::Password => {
+                            self.password.handle_event(&event);
+                        }
+                    }
                     None
                 }
             }
