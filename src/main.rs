e42f6e8b300e6fa7767802b254530928b952db0e
tlipinski <me@tlipinski.net>
2026-02-23 18:48:51 +0100

    Remove `gui_cmd` module and integrate its functionality directly into `main.rs` for simplified code structure.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 #![allow(unused)]
 
-mod cli;
 mod config;
 mod secret;
 mod ui;
 
-use crate::cli::gui_cmd::handle_gui_cmd;
 use crate::config::get_config;
 use crate::secret::retrieve;
 use anyhow::{Error, Result};
@@ -15,6 +13,7 @@ use log::{LevelFilter, error, info, warn};
 use osb::user_info::get_user_info;
 use std::fs::OpenOptions;
 use std::path::PathBuf;
+use std::process::exit;
 use ui::app::App;
 
 #[tokio::main]
@@ -57,4 +56,55 @@ async fn run(args: Args) -> Result<()> {
     let _ = get_config()?;
 
     handle_gui_cmd(args.name.as_deref()).await
+}
+
+async fn handle_gui_cmd(path_opt: Option<&str>) -> Result<()> {
+    let p = if let Some(path) = path_opt {
+        let canon_res = PathBuf::from(&path).canonicalize();
+
+        match canon_res {
+            Ok(canon) => {
+                if (canon.is_absolute()) {
+                    canon
+                } else {
+                    let current_dir = std::env::current_dir()?;
+                    info!("cwd: {}", current_dir.display());
+
+                    current_dir.join(path)
+                }
+            }
+            Err(err) => {
+                warn!("{err}");
+                let current_dir = std::env::current_dir()?;
+                info!("cwd: {}", current_dir.display());
+                current_dir
+            }
+        }
+    } else {
+        let current_dir = std::env::current_dir()?;
+        info!("cwd: {}", current_dir.display());
+        current_dir
+    };
+
+    info!("Input path: {:?}", p);
+
+    let (base_path, file_name) = if (p.is_dir()) {
+        (Some(p.as_path()), None)
+    } else {
+        (p.parent(), p.file_stem().and_then(|os_str| os_str.to_str()))
+    };
+
+    if let Some(bp) = base_path{
+        let mut terminal = ratatui::init();
+        info!("Base path: {:?}", bp);
+        info!("File name: {:?}", file_name);
+        App::run(&mut terminal, bp, file_name).await;
+        ratatui::restore();
+
+        Ok(())
+    } else {
+        error!("Invalid path: {:?}", base_path);
+        exit(1)
+    }
+
 }
