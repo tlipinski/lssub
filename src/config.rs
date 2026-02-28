4cbc0faf11ac96f48a718b3772d82e91c2dca6b8
tlipinski <me@tlipinski.net>
2026-02-28 20:16:14 +0100

    Improve logging in `get_config`: add config path and format debug output

--- src/config.rs
+++ src/config.rs
@@ -17,10 +17,10 @@ impl Default for Config {
 }
 
 pub fn get_config() -> Result<Config> {
-    info!("Loading config");
     let xdg_dirs = xdg::BaseDirectories::with_prefix("subster")?;
     debug!("XDG: {:?}", xdg_dirs);
     let config_path = xdg_dirs.get_config_file("config.toml");
+    info!("Loading config from: {:?}", config_path);
     if config_path.exists() {
         let contents = match fs::read_to_string(config_path) {
             Ok(raw) => raw,
@@ -30,7 +30,7 @@ pub fn get_config() -> Result<Config> {
             }
         };
         let config: Config = toml::from_str(&contents)?;
-        info!("Config loaded {:?}", config);
+        info!("Config loaded: {config:?}");
         Ok(config)
     } else {
         let default = Config::default();
