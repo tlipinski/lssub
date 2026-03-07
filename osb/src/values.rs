069bcdc7bb3daa29ca839ec69db387591548a2b5
tlipinski <me@tlipinski.net>
2026-03-07 19:55:56 +0100

    Refactor project structure: enable workspace configuration, bind version/edition to workspace, and dynamically set `USER_AGENT` version.

--- osb/src/values.rs
+++ osb/src/values.rs
@@ -1,4 +1,4 @@
 pub const API_URL: &str = "https://api.opensubtitles.com/api/v1";
 pub const VIP_API_URL: &str = "https://vip-api.opensubtitles.com/api/v1";
-pub const USER_AGENT: &str = "lssub v0.0.1";
+pub const USER_AGENT: &str = concat!("lssub v", env!("CARGO_PKG_VERSION"));
 pub const AK: &str = "xNSag5DdRlR219FYSwkCjQOUDuXKX1CE";
