0c30ea918a713e19136405345ee3b656a821568c
tlipinski <me@tlipinski.net>
2026-03-08 11:54:40 +0100

    Refactor `spinner_task`: rename variables for clarity, reorder imports, and simplify spinner character updates.

--- src/ui/spinner.rs
+++ src/ui/spinner.rs
@@ -1,21 +1,20 @@
-use std::sync::{Arc, RwLock};
 use crate::ui::actions::Action;
+use std::sync::{Arc, RwLock};
 use tokio::sync::broadcast::Receiver;
 use tokio::sync::mpsc::Sender;
 use tokio::time::{Duration, sleep};
 
 pub struct Spinner {
-    pub c: char
+    pub c: char,
 }
 
-pub async fn spinner_task(s: Arc<RwLock<Spinner>>) -> anyhow::Result<()> {
-    let spinner = ['|', '/', '-', '\\'];
+pub async fn spinner_task(spinner: Arc<RwLock<Spinner>>) -> anyhow::Result<()> {
+    let chars = ['|', '/', '-', '\\'];
     let mut pos = 0;
     loop {
         sleep(Duration::from_millis(200)).await;
         pos += 1;
-        pos %= spinner.len();
-        let ch = spinner[pos];
-        s.write().unwrap().c = ch;
+        pos %= chars.len();
+        spinner.write().unwrap().c = chars[pos];
     }
 }
