1598c92f998f2e1f651e4c84a5948dadea1a4401
tlipinski <git@t3xh.nl>
2026-03-14 22:31:45 +0100

    Refactor `handle_key_event` in `App`: simplify logic using `or_else` for cleaner delegation to `active_widget` and key matching.

--- src/ui/app.rs
+++ src/ui/app.rs
@@ -327,42 +327,41 @@ impl App {
     }
 
     fn handle_key_event(&mut self, event: &Event) -> Option<Action> {
-        if let Some(action) = self.active_widget().handle_key_event(event) {
-            Some(action)
-        } else {
-            if let Event::Key(key_event) = event {
-                match key_event {
-                    KeyEvent {
-                        code: KeyCode::Esc,
-                        ..
-                    } => Some(SwitchScreen(Search)),
-                    KeyEvent {
-                        code: KeyCode::F(2),
-                        ..
-                    } => Some(SwitchScreen(Search)),
-                    KeyEvent {
-                        code: KeyCode::F(3),
-                        ..
-                    } => Some(SwitchScreen(Account)),
-                    KeyEvent {
-                        code: KeyCode::F(4),
-                        ..
-                    } => Some(SwitchScreen(Language)),
-                    KeyEvent {
-                        code: KeyCode::F(10),
-                        ..
-                    } => Some(Exit),
-                    KeyEvent {
-                        code: KeyCode::F(12),
-                        ..
-                    } => Some(SwitchScreen(About)),
+        self.active_widget()
+            .handle_key_event(event)
+            .or_else(|| {
+                if let Event::Key(key_event) = event {
+                    match key_event {
+                        KeyEvent {
+                            code: KeyCode::Esc, ..
+                        } => Some(SwitchScreen(Search)),
+                        KeyEvent {
+                            code: KeyCode::F(2),
+                            ..
+                        } => Some(SwitchScreen(Search)),
+                        KeyEvent {
+                            code: KeyCode::F(3),
+                            ..
+                        } => Some(SwitchScreen(Account)),
+                        KeyEvent {
+                            code: KeyCode::F(4),
+                            ..
+                        } => Some(SwitchScreen(Language)),
+                        KeyEvent {
+                            code: KeyCode::F(10),
+                            ..
+                        } => Some(Exit),
+                        KeyEvent {
+                            code: KeyCode::F(12),
+                            ..
+                        } => Some(SwitchScreen(About)),
 
-                    _ => None,
+                        _ => None,
+                    }
+                } else {
+                    None
                 }
-            } else {
-                None
-            }
-        }
+            })
     }
 }
 
