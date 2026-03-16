e4350d5ad123af20845bc5d98061dd958e2f9b2c
tlipinski <git@t3xh.nl>
2026-03-16 17:18:30 +0100

    Refactor `account_widget`: replace `TaskRunner` with `RunTask` action, update `Init` action logic, and remove unused `refresh` method.

--- src/ui/account_widget.rs
+++ src/ui/account_widget.rs
@@ -2,9 +2,7 @@ use crate::osb::login::login;
 use crate::osb::user_info::{UserInfo, get_user_info};
 use crate::secret::{clear, retrieve, store};
 use crate::ui::actions::Action;
-use crate::ui::actions::Action::{
-    ChangeStatus, ReceivedInput, SwitchScreen, UserLoggedIn, UserLoggedOut,
-};
+use crate::ui::actions::Action::{ChangeStatus, ReceivedInput, RunTask, SwitchScreen, UserLoggedIn, UserLoggedOut};
 use crate::ui::component::Component;
 use crate::ui::logged_in_widget::LoggedInWidget;
 use crate::ui::login_widget::LoginWidget;
@@ -15,32 +13,49 @@ use log::{error, info};
 use ratatui::Frame;
 use ratatui::layout::Rect;
 use ratatui::text::ToSpan;
+use Action::Init;
 
 pub struct AccountWidget {
     login_widget: LoginWidget,
     logged_in_widget: LoggedInWidget,
-    task_runner: TaskRunner,
     logged_in: bool,
 }
 
 impl Component for AccountWidget {
     fn update(&mut self, action: &Action) -> Option<Action> {
         match action {
-            Action::Init => {
-                self.refresh(); // todo side effect
+            Init => {
+                Some(RunTask(Task::new(async move {
+                    match retrieve().await {
+                        Ok(Some(jwt)) => match get_user_info(&jwt).await {
+                            Ok(user_info) => Ok(UserLoggedIn(user_info)),
+                            Err(e) => {
+                                error!("Error getting user info: {e}");
+                                Err(Error::msg(e.to_string()))
+                            }
+                        },
+                        Ok(None) => Ok(ChangeStatus("".into())), // todo
+                        Err(e) => {
+                            error!("Error retrieving jwt: {e}");
+                            Err(Error::msg(e.to_string()))
+                        }
+                    }
+                })))
             }
             UserLoggedIn(user_info) => {
                 self.logged_in = true;
                 self.logged_in_widget.user_info = user_info.clone();
+                None
             }
             UserLoggedOut => {
                 self.logged_in = false;
                 self.logged_in_widget.user_info = UserInfo::default();
+                None
             }
-            _ => {}
+            _ => {
+                None
+            }
         }
-
-        None
     }
 
     fn handle_key_event(&mut self, event: &Event) -> Option<Action> {
@@ -70,7 +85,6 @@ impl AccountWidget {
                 },
                 task_runner.clone(),
             ),
-            task_runner,
             logged_in: false,
         }
     }
@@ -83,22 +97,4 @@ impl AccountWidget {
         }
     }
 
-    pub fn refresh(&mut self) {
-        self.task_runner.run(Task::new(async move {
-            match retrieve().await {
-                Ok(Some(jwt)) => match get_user_info(&jwt).await {
-                    Ok(user_info) => Ok(UserLoggedIn(user_info)),
-                    Err(e) => {
-                        error!("Error getting user info: {e}");
-                        Err(Error::msg(e.to_string()))
-                    }
-                },
-                Ok(None) => Ok(ChangeStatus("".into())), // todo
-                Err(e) => {
-                    error!("Error retrieving jwt: {e}");
-                    Err(Error::msg(e.to_string()))
-                }
-            }
-        }));
-    }
 }
