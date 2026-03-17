e35e2ac049abac75d8429435c20dfda740a9888a
tlipinski <git@t3xh.nl>
2026-03-17 23:34:14 +0100

    Refactor: handle `InputReceived` action in `update` for improved action management consistency

--- src/ui/app.rs
+++ src/ui/app.rs
@@ -137,8 +137,7 @@ impl App {
 
     fn update(&mut self, action: &Action) -> Option<Action> {
         match action {
-            Tick => {}
-            Multi(_) => {}
+            Tick | Multi(_) | InputReceived(_) => {}
             _ => debug!("--- {:?}", action),
         };
 
