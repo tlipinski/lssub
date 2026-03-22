9ac65baab018dbd452c072480fb1d7881b6cb036
tlipinski <git@t3xh.nl>
2026-03-22 19:03:01 +0100

    Add snapshot test for `subs_fetched`, extend `app.rs` with fetched subtitles scenario

--- src/ui/app.rs
+++ src/ui/app.rs
@@ -348,6 +348,7 @@ mod tests {
     use ratatui::backend::TestBackend;
     use ratatui::Terminal;
     use insta::assert_snapshot;
+    use crate::osb::subtitles::{Attributes, FeatureDetails, Subtitle};
     use crate::osb::user_info::User;
     use super::*;
 
@@ -408,6 +409,65 @@ mod tests {
 
         app.update(&UserLoggedIn(user));
         app.update(&SwitchScreen(Account));
+
+        let mut terminal = Terminal::new(TestBackend::new(100, 20)).unwrap();
+        terminal.draw(|frame| app.draw(frame)).unwrap();
+
+        assert_snapshot!(terminal.backend());
+    }
+
+    #[tokio::test]
+    async fn subs_fetched() {
+        let mut app = App::new(Path::new("."), None);
+
+        let subs = vec![
+            Subtitle {
+                id: "1234".to_string(),
+                r#type: "".to_string(),
+                attributes: Attributes {
+                    feature_details: FeatureDetails {
+                        feature_id: 0,
+                        title: "title".to_string(),
+                        movie_name: "movie name".to_string(),
+                        year: Some(2004),
+                        parent_feature_id: None,
+                        parent_title: None,
+                    },
+                    language: "en".to_string(),
+                    download_count: 4,
+                    new_download_count: 8,
+                    ai_translated: true,
+                    votes: 8,
+                    upload_date: "2024-04-24T10:10:10".to_string(),
+                    release: "release".to_string(),
+                    files: vec![],
+                },
+            },
+
+            Subtitle {
+                id: "1243".to_string(),
+                r#type: "".to_string(),
+                attributes: Attributes {
+                    feature_details: FeatureDetails {
+                        feature_id: 0,
+                        title: "title 2".to_string(),
+                        movie_name: "movie name 2".to_string(),
+                        year: None,
+                        parent_feature_id: None,
+                        parent_title: None,
+                    },
+                    language: "pl".to_string(),
+                    download_count: 8,
+                    new_download_count: 14,
+                    ai_translated: false,
+                    votes: 0,
+                    upload_date: "2014-04-24T10:10:10".to_string(),
+                    release: "release 2".to_string(),
+                    files: vec![],
+                },
+            }
+        ];
+        app.update(&SubtitlesFetched(subs));
 
         let mut terminal = Terminal::new(TestBackend::new(100, 20)).unwrap();
         terminal.draw(|frame| app.draw(frame)).unwrap();
